3e58312b8bddc4e1f4322a3aade274ce34bd05d6
CarolVilarino <carol_vilarino@hotmail.com>
2022-05-27 12:03:18 -0300

    alteração de identação para melhor visualização e do nome do banco na query

--- Perguntas-e-consultas-Sabrina.sql
+++ Perguntas-e-consultas-Sabrina.sql
@@ -1,9 +1,13 @@
 -- Qual é o jogo e sua desenvolvedora que tem os 3 jogos mais caros e suas avaliações?
-select price, name, developer, positive_ratings, negative_ratings from steam order by price desc limit 3;
+SELECT price, name, developer, positive_ratings, negative_ratings 
+FROM steam order by price desc limit 3;
 
 -- Quais desenvolvedores tem mais avaliação positivas/negativas? 
-select developer, sum(positive_ratings) aval_posit from steam group by developer order by aval-posit desc;
-select developer, sum(negative_ratings) aval_negat from steam group by developer order by aval_negat desc;
+SELECT developer, sum(positive_ratings) aval_posit 
+FROM steam group by developer order by aval-posit desc;
+SELECT developer, sum(negative_ratings) aval_negat 
+FROM steam group by developer order by aval_negat desc;
 
 -- Quais as plataformas que tem mais disponibilidade? 
-select platforms, count(platforms) contador from steam group by platforms order by contador desc;
+SELECT platforms, count(platforms) contador 
+FROM steam group by platforms order by contador desc;
